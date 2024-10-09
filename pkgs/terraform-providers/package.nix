4c5960a3387442674e850dcabbd64102e9577ccf
vdbewout <vdbewout@gmail.com>
2024-10-09 12:45:09 +0000

    feat: add terraform provider sodium

---
+++ pkgs/terraform-providers/package.nix
@@ -0,0 +1,63 @@
+{
+  lib,
+  terraform-providers,
+  writeShellApplication,
+  nix-update,
+  curlMinimal,
+  jq,
+}:
+let
+  inherit (lib.attrsets) mapAttrs;
+  inherit (lib.trivial) importJSON;
+
+  inherit (terraform-providers) mkProvider;
+
+  list = importJSON ./providers.json;
+
+  providerFile = "./pkgs/terraform-providers/providers.json";
+  updateProvider = writeShellApplication {
+    name = "terraform-provider-get-lastest-version";
+    runtimeInputs = [
+      curlMinimal
+      jq
+      nix-update
+    ];
+    text = ''
+      provider="$1"
+      homepage="$(jq -r ".''${provider}.homepage" ${providerFile})"
+      current="$(jq -r ".''${provider}.rev" ${providerFile})"
+
+      latest="$(curl -s "''${homepage//providers/v1/providers}" | jq -r '.tag')"
+      if [[ "$current" == "$latest" ]]; then
+        # Same version nothing todo
+        exit
+      fi
+
+      # Update version
+      tempfile="$(mktemp)"
+      jq ".''${provider}.rev = \"''${latest}\"" ${providerFile} > "''${tempfile}"
+      cp "''${tempfile}" ${providerFile}
+      rm "''${tempfile}"
+
+      # Update hashes
+      nix-update --override-filename=${providerFile} --version=skip "terraform-providers.''${provider}"
+    '';
+  };
+
+  automated-providers = mapAttrs (
+    providerName: providerAttrs:
+    (mkProvider providerAttrs).overrideAttrs {
+      passthru.updateScript = [
+        (lib.getExe updateProvider)
+        providerName
+      ];
+
+    }
+  ) list;
+
+  # These are the providers that don't fall in line with the default model
+  special-providers = { };
+
+  actualProviders = automated-providers // special-providers;
+in
+actualProviders
