3e61ebc81e8fc244e40ff2801bc21e91f1ef4a50
vdbe <vdbewout@gmail.com>
2025-08-10 15:08:26 +0200

    chore: add shell.nix

---
+++ shell.nix
@@ -0,0 +1,36 @@
+{
+  system ? builtins.currentSystem,
+  ...
+}:
+
+let
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  root = lock.nodes.${lock.root};
+
+  inherit (lock.nodes.${root.inputs.flake-compat}.locked)
+    owner
+    repo
+    rev
+    narHash
+    ;
+
+  flake-compat = fetchTarball {
+    url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+    sha256 = narHash;
+  };
+
+  flake = import flake-compat {
+    inherit system;
+    # hack to skip fetchGit when evaluating impurely and get original paths
+    src = {
+      outPath = ./.;
+    };
+  };
+
+  # default.defaultNix.inputs.nixpkgs.lib
+
+  self = flake.defaultNix;
+
+  # inherit (self.inputs.nixpkgs) lib;
+in
+self.inputs.nixpkgs.legacyPackages.${system}.mkShellNoCC { }
